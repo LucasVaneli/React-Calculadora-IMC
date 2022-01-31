a553ed7555fec299c7f428fa6db46fcf11685053
Lucas Vanneli <vaneli.lucas99@gmail.com>
2022-01-31 19:56:07 -0300

    criado calculadora que faz calculo de IMC com react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useState } from 'react';
+import './app.css';
+
+export default function App() {
+
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  const [mensagem, setMensagem] = useState('');
+
+  function calcularIMC() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (imc < 18.6) {
+      setMensagem('Você está abaixo do peso! seu IMC: ' + imc.toFixed(2))
+    } else if (imc >= 18.6 && imc < 24.9) {
+      setMensagem('Peso ideial! seu IMC: ' + imc.toFixed(2))
+    } else if (imc >= 24.9 && imc < 34.9) {
+      setMensagem('Um pouco acima do peso! seu IMC: ' + imc.toFixed(2))
+    } else if (imc < 34.9) {
+      setMensagem('Obesidade! seu IMC: ' + imc.toFixed(2))
+    }
+  }
+
+  return(
+    <div className='app'>
+      <h1>Calculadora IMC</h1>
+      <span>
+        Vamos Calcular o seu IMC
+      </span>
+      <div className='area-input'>
+        <input type='text' 
+          placeholder="Peso em (KG)" 
+          value={peso} onChange={ (e) => setPeso(e.target.value) }>
+        </input>     
+        <input type='text' 
+        placeholder="Altura em (KG)" 
+        value={altura} onChange={ (e) => setAltura(e.target.value) }>
+        </input>     
+
+        <button onClick={calcularIMC}>
+          Calcular
+        </button>
+
+      </div>
+
+      <h2>{mensagem}</h2>
+
     </div>
-  );
+  )
 }
-
-export default App;
